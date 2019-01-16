4135482ed91072266fbdac1789ffb6b37dc3e273
Walter Huf <hufman@gmail.com>
2019-01-15 22:12:37 -0800

    Adds Google Maps in the projection

--- app/src/main/java/me/hufman/testprojection/MainProjection.kt
+++ app/src/main/java/me/hufman/testprojection/MainProjection.kt
@@ -1,41 +1,64 @@
 package me.hufman.testprojection
 
-import android.Manifest
 import android.app.Presentation
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.Display
 import android.view.WindowManager
+import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.projection.*
 
 class MainProjection(parentContext: Context, display: Display): Presentation(parentContext, display) {
 	val TAG = "TestProjection"
+	var map: GoogleMap? = null
 	val colors = ArrayList<Int>()
 	var colorIndex = 0
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
+		val r = context.resources
 		colors.add(Color.CYAN)
 		colors.add(Color.BLUE)
 		colors.add(Color.RED)
 
 		window.setType(WindowManager.LayoutParams.TYPE_PRIVATE_PRESENTATION)
-
+//
 		setContentView(R.layout.projection)
+
+		mapViewProjection.onCreate(savedInstanceState)
+		mapViewProjection.getMapAsync {
+			Log.i(TAG, "Got map!")
+			map = it
+			it.isIndoorEnabled = false
+			it.isTrafficEnabled = true
+
+		}
 
 	}
 
 	override fun onStart() {
 		super.onStart()
 		Log.i(TAG, "Projection Start")
+		mapViewProjection.onStart()
+		mapViewProjection.onResume()
 	}
 
 	override fun onStop() {
 		super.onStop()
 		Log.i(TAG, "Projection Stopped")
+		mapViewProjection.onPause()
+		mapViewProjection.onStop()
+		mapViewProjection.onDestroy()
+	}
+
+	override fun onSaveInstanceState(): Bundle {
+		val output = super.onSaveInstanceState()
+		mapViewProjection.onSaveInstanceState(output)
+		return output
 	}
 
 	fun changeColor() {
